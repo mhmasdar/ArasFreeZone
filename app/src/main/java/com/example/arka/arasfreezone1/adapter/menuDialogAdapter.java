280f4932cc7f8b175ddbc3316e7127f547a0685b
mhmasdar <probit.microlab@yahoo.com>
2017-12-12 22:33:19 +0330

    masdar added changes

--- app/src/main/java/com/example/arka/arasfreezone1/adapter/menuDialogAdapter.java
+++ app/src/main/java/com/example/arka/arasfreezone1/adapter/menuDialogAdapter.java
@@ -5,9 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.TextView;
 
 import com.example.arka.arasfreezone1.R;
 
@@ -15,28 +12,28 @@ import com.example.arka.arasfreezone1.R;
  * Created by mohamadHasan on 12/12/2017.
  */
 
-public class facilityDialogAdapter extends RecyclerView.Adapter<facilityDialogAdapter.myViewHolder> {
+public class menuDialogAdapter extends RecyclerView.Adapter<menuDialogAdapter.myViewHolder> {
 
     private Context context;
     private LayoutInflater mInflater;
 
 
 
-    public facilityDialogAdapter(Context context) {
+    public menuDialogAdapter(Context context) {
         this.context = context;
         mInflater = LayoutInflater.from(context);
     }
 
     @Override
-    public facilityDialogAdapter.myViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.item_facility_list, parent, false);
-        facilityDialogAdapter.myViewHolder holder = new facilityDialogAdapter.myViewHolder(view);
+    public menuDialogAdapter.myViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_menu_list, parent, false);
+        menuDialogAdapter.myViewHolder holder = new menuDialogAdapter.myViewHolder(view);
         return holder;
     }
 
 
     @Override
-    public void onBindViewHolder(facilityDialogAdapter.myViewHolder holder, int position) {
+    public void onBindViewHolder(menuDialogAdapter.myViewHolder holder, int position) {
     }
 
     @Override
